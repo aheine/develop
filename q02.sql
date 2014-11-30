8e3c9ec66c057938e31d0fb2ae3e14cda9327c50
aheine <aheine@uvm.edu>
2014-11-30 15:27:35 -0500

    Changed statement slightly

--- q02.sql
+++ q02.sql
@@ -1,1 +1,1 @@
-SELECT * FROM tblPerson, tblBills, tblHousehold
+SELECT pmkHouseholdName AS 'Household', pmkUsername AS 'User', fldEmail AS 'Email', fldMonth AS 'Month', fldType AS 'Type of Expense', pmkBillName AS 'Name of Expense', fldAmount AS 'Amount', fldPaid AS 'Paid', fldCash AS 'Cash', fldCredit AS 'Credit', fldCheck AS 'Check', fldLost AS 'Lost' FROM tblPerson, tblBills, tblHousehold 
